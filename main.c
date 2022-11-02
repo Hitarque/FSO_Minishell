932ae547a4fb1225014e79b0190209073ccf00ae
Hitarque <60928575+Hitarque@users.noreply.github.com>
2022-11-02 20:01:20 +0100

    update 1

---
+++ main.c
@@ -0,0 +1,34 @@
+//FSO project 
+
+void main(){
+
+    //0| While true:
+            //creation de fork for each imput
+
+    // 1|Verification des parametres In: argv +argc Display de l'erreuret dire que seuelment 1 parametre de pris
+
+    // 2|Interpretation de la commande
+        // 2.1| Cat verification de la validité des parametres
+                //-in: parametres de cat
+                //-out: result / boolean
+        // 2.2| ls verification de la validité des parametres
+                //-in: parametres de ls
+                //-out: result / boolean
+        // 2.3| exit  
+                //just exit 
+
+    //execut in fork:
+                    //execut command + display command
+                    //if false display error
+                    //else display result
+
+                    //pere await fin de fork fils
+
+    
+
+    
+    
+
+
+}
+
